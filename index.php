56db7ed6224a1b594334a38a3e0e5f6e5ed052a5
TobyDevOps <Tngoetz@aol.com>
2024-01-23 12:40:24 -0500

    updated index, images for diner

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
 
+/* Toby Goetz
+ * 1/16/2024
+ * https://tgoetzgr.greenriverdev.com/SDEV328/diner/index.php
+ * This is my CONTROLLER for the Diner App
+ */
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once ('vendor/autoload.php');
+
+// Instantiate Fat-Free framework (F3)
+$f3 = Base::instance(); // Static Method
+
+// Define a default route
+$f3->route('GET /', function() {
+    //echo 'hi';
+    echo "Breakfast";
+
+    //Display a view page
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+
+});
+
+// Run Fat-Free
+$f3->run(); // Instance Method
+
+
 echo '<h1>Diner</h1>';
